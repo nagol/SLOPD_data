faf2774141f93c15cbb5ac182fcb7ce904f46261
Logan Lossing <loganlossing@gmail.com>
2021-11-11 17:31:59 -0800

    Used black for linting

--- scripts/address_lookup.py
+++ scripts/address_lookup.py
@@ -1,5 +1,3 @@
-
-
 import pandas as pd
 from geopy.geocoders import Nominatim
 
@@ -8,37 +6,48 @@ from geopy.geocoders import Nominatim
 pd_data = pd.read_csv("data/csv/SLOPD_report.csv")
 
 
-def get_address_geoinfo(police_data, user_agent = "SLOPD_parser", verbose = False):
+def get_address_geoinfo(police_data, user_agent="SLOPD_parser", verbose=False):
 
-    addresses = police_data.address.str.split(';', expand = True)
-    addresses.rename(columns = {0:'street_address', 1:'address_description'}, inplace = True)
-    addresses.street_address = addresses.street_address.str.split('#').str[0] # remove unit numbers from address, ie 215 Chorro #15
-    
+    addresses = police_data.address.str.split(";", expand=True)
+    addresses.rename(
+        columns={0: "street_address", 1: "address_description"}, inplace=True
+    )
+    addresses.street_address = addresses.street_address.str.split("#").str[
+        0
+    ]  # remove unit numbers from address, ie 215 Chorro #15
+
     geolocator = Nominatim(user_agent="SLOPD_parser")
 
     location_dict = {}
 
     for i, address in enumerate(addresses.street_address):
 
+        try:
 
-            try:
+            location = geolocator.geocode(f"{address}, San Luis Obispo, CA")
+            location_dict[i] = {
+                "address": location.address,
+                "latitude": location.latitude,
+                "longitude": location.longitude,
+            }
 
-                location = geolocator.geocode(f"{address}, San Luis Obispo, CA")
-                location_dict[i] = {'address': location.address, 'latitude': location.latitude, 'longitude':location.longitude} 
+            if verbose:
+                print(location.address)
+                print((location.latitude, location.longitude))
+                print(location.raw)
 
-                if verbose:
-                    print(location.address)
-                    print((location.latitude, location.longitude))
-                    print(location.raw)
+        except:
 
-                
-            except:
-                
-                if verbose:
-                    print('No results returned. Either address not found or input address malformed or ambiguous.')
+            if verbose:
+                print(
+                    "No results returned. Either address not found or input address malformed or ambiguous."
+                )
 
-                location_dict[i] = {'address': None, 'latitude': None, 'longitude': None} 
+            location_dict[i] = {"address": None, "latitude": None, "longitude": None}
 
     return location_dict
 
-location_dict = get_address_geoinfo(police_data = pd_data.head(), user_agent = "SLOPD_parser", verbose = True)
+
+location_dict = get_address_geoinfo(
+    police_data=pd_data.head(), user_agent="SLOPD_parser", verbose=True
+)
--- scripts/analysis.py
+++ scripts/analysis.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -10,13 +9,19 @@ pd_data = pd.read_csv("data/csv/SLOPD_report.csv")
 pd_data.type = pd_data.type.str.strip()
 
 # Tabulate by call type - i.e. ROBBERY, ASSULT, DUI,...
-type_table = pd_data[['type']].groupby(['type']).size().reset_index().sort_values(by = [0], ascending=[False])
-type_table.rename(columns = {0:'count'}, inplace=True)
+type_table = (
+    pd_data[["type"]]
+    .groupby(["type"])
+    .size()
+    .reset_index()
+    .sort_values(by=[0], ascending=[False])
+)
+type_table.rename(columns={0: "count"}, inplace=True)
 type_table.type = type_table.type.str.upper()
 
 # Count the calls per day, most recent day may be imcomplete so remove
-calls_per_day_table = pd_data[['date']].groupby('date').size().reset_index()
-calls_per_day_table.rename(columns = {0: 'count'}, inplace = True)
+calls_per_day_table = pd_data[["date"]].groupby("date").size().reset_index()
+calls_per_day_table.rename(columns={0: "count"}, inplace=True)
 calls_per_day_table.head()
 
 # Initialize the matplotlib figure for barchart ----
@@ -25,30 +30,33 @@ f, ax = plt.subplots(figsize=(7, 15))
 # Plot frequency of each type of call
 sns.set_color_codes("pastel")
 sns.barplot(
-    x="count", 
-    y="type", 
-    data = type_table,
-    order = type_table.sort_values('count', ascending=False).type,
+    x="count",
+    y="type",
+    data=type_table,
+    order=type_table.sort_values("count", ascending=False).type,
     color="b",
-    ci = None)
-ax.set(ylabel="",
-       xlabel="Count")
+    ci=None,
+)
+ax.set(ylabel="", xlabel="Count")
 sns.despine(left=True, bottom=True)
 
 
 # Save barchart
-f.savefig('img/barchart.png', bbox_inches='tight')
+f.savefig("img/barchart.png", bbox_inches="tight")
 
 # Initialize the matplotlib figure for time-series plot ----
 f, ax = plt.subplots(figsize=(15, 7))
 
 sns.lineplot(
-    x="date", 
+    x="date",
     y="count",
     markers=True,
-    data=calls_per_day_table[calls_per_day_table.date != calls_per_day_table.date.max()])
+    data=calls_per_day_table[
+        calls_per_day_table.date != calls_per_day_table.date.max()
+    ],
+)
 ax.set(ylim=(0, None))
 plt.setp(ax.get_xticklabels(), rotation=90)
 
 # Save time-series plot
-f.savefig('img/time_series_plot.png', bbox_inches='tight')
+f.savefig("img/time_series_plot.png", bbox_inches="tight")
