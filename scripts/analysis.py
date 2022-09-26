3b012abd1e182ca71d25ac8135f4bb7a76a65b4b
Logan Lossing <loganlossing@gmail.com>
2022-09-25 17:45:36 -0700

    Fixing some plotting issues [adjusting date axis on time-series plot, reducing to top-50 categories in the barplot]

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.dates as md
 
 sns.set_theme(style="whitegrid")
 
 # Grab data from repo
 pd_data = pd.read_csv("data/csv/SLOPD_report.csv")
 pd_data.type = pd_data.type.str.strip()
+pd_data['date'] = pd.to_datetime(pd_data['date'], format = '%m/%d/%y')
 
 # Tabulate by call type - i.e. ROBBERY, ASSULT, DUI,...
 type_table = (
@@ -18,6 +20,8 @@ type_table = (
 )
 type_table.rename(columns={0: "count"}, inplace=True)
 type_table.type = type_table.type.str.upper()
+type_table = type_table.head(50)
+print(type_table)
 
 # Count the calls per day, most recent day may be imcomplete so remove
 calls_per_day_table = pd_data[["date"]].groupby("date").size().reset_index()
@@ -56,6 +60,21 @@ sns.lineplot(
     ],
 )
 ax.set(ylim=(0, None))
+
+# specify the position of the major ticks at the beginning of the week
+ax.xaxis.set_major_locator(md.WeekdayLocator(byweekday = 1))
+# specify the format of the labels as 'year-month-day'
+ax.xaxis.set_major_formatter(md.DateFormatter('%m/%d/%y'))
+# (optional) rotate by 90° the labels in order to improve their spacing
+plt.setp(ax.xaxis.get_majorticklabels(), rotation = 90)
+
+# specify the position of the minor ticks at each day
+ax.xaxis.set_minor_locator(md.DayLocator(interval = 1))
+
+# set ticks length
+ax.tick_params(axis = 'x', which = 'major', length = 10)
+ax.tick_params(axis = 'x', which = 'minor', length = 5)
+
 plt.setp(ax.get_xticklabels(), rotation=90)
 
 # Save time-series plot
